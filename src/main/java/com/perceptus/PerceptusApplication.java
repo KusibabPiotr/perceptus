402685b431e61f21b9d7443d611f87e64d4b092e
Piotr Kusibab <piotr.kusibab@amway.com>
2023-05-09 14:20:03 +0200

    initial project configuration

---
+++ src/main/java/com/perceptus/PerceptusApplication.java
@@ -0,0 +1,13 @@
+package com.perceptus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerceptusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerceptusApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/perceptus/model/Book.java
@@ -0,0 +1,4 @@
+package com.perceptus.model;
+
+public class Book {
+}
