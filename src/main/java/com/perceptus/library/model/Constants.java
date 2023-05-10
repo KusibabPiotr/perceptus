b73317b0a7cd6e251e44c2702420239b7a1d47be
Piotr Kusibab <piotr.kusibab@amway.com>
2023-05-10 13:19:45 +0200

    working on JwtAuthFilter and UserDetails service

---
+++ src/main/java/com/perceptus/library/model/Constants.java
@@ -0,0 +1,8 @@
+package com.perceptus.library.model;
+
+import lombok.Getter;
+
+@Getter
+public class Constants {
+    public static final String BEARER = "Bearer ";
+}
---
+++ src/main/java/com/perceptus/library/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.perceptus.library.security.filter;
+
+import com.perceptus.library.service.JwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+import static com.perceptus.library.model.Constants.BEARER;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader(AUTHORIZATION);
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith(BEARER)){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
+}
