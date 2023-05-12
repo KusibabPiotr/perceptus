ec55b91ac0b37851586c8020033121670c45dcc0
Piotr Kusibab <piotr.kusibab@amway.com>
2023-05-12 14:49:58 +0200

    added parametrized sql query in jparepository

--- src/main/java/com/perceptus/library/repository/TokenRepository.java
+++ src/main/java/com/perceptus/library/repository/TokenRepository.java
@@ -6,15 +6,12 @@ import java.util.Optional;
 import com.perceptus.library.model.domain.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
-    @Query(value = """
-      select t from Token t inner join User u\s
-      on t.user.id = u.id\s
-      where u.id = :id and (t.expired = false or t.revoked = false)\s
-      """)
-    List<Token> findAllValidTokenByUser(Integer id);
+    @Query("select t from Token t inner join User u on t.user.id = u.id where u.id = :id and (t.expired = false or t.revoked = false)")
+    List<Token> findAllValidTokenByUser(@Param("id") Integer id);
 
     Optional<Token> findByToken(String token);
 }
